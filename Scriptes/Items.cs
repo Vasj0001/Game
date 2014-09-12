196059c8f0dee286d22a5f0feae76ce98d77aed8
xen0987 <klimovjeka0987@yandex.ru>
2014-09-12 22:17:53 +0400

    Инициализация типов и раритетности предметов

--- Scriptes/Items.cs
+++ Scriptes/Items.cs
@@ -2,28 +2,23 @@
 using System.Collections;
 
 public class Items : MonoBehaviour {
-	public GameObject itemInfoPanel;
-	public int weaponDMG;
-	public UILabel itemInfoLabel;
 
-	// Use this for initialization
-	void Start () {
-	
+
+	public enum rarity{
+		Common,
+		Rare,
+		Mystical;
+		Legendary;
+		Set
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (NGUITools.GetActive(itemInfoPanel)){
-			itemInfoLabel.text="Basic Sword\nDamage 2\nAttack Speed 0.5";
-			itemInfoPanel.transform.localPosition=new Vector3(Input.mousePosition.x+20,Input.mousePosition.y-720,0);
-		}
-		if(Input.GetKeyDown(KeyCode.Mouse0))
-			NGUITools.SetActive(itemInfoPanel,false);
-	}
-	void item_InfoF(){
-		NGUITools.SetActive(itemInfoPanel,false);
-	}
-	void item_InfoT(){
-		NGUITools.SetActive(itemInfoPanel,true);
+	public enum type{
+		Weapon,
+		Left Hand,
+		Armor,
+		Ring,
+		Amulet,
+		Potion,
+		Quest,
+		Other
 	}
 }
