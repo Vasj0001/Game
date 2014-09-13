361b869f6518aca9f1f9a8820af3e2c366cb34ac
xen0987 <klimovjeka0987@yandex.ru>
2014-09-13 11:27:32 +0400

    Первая ячейка инвентаря

---
+++ Scriptes/GUI_Inv.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class GUI_Inv : MonoBehaviour {
+	
+	private Rect[] rect = new Rect[10];
+	void Start(){
+	
+	}
+	void Update(){
+		rect[0] = new Rect (29,552,51,51)
+	}
+}
